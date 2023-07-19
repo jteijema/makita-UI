9bfe29447875bf7fec36f2df2ba04a2b819ff2c7
JT <j.j.teijema@uu.nl>
2023-07-20 01:18:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="Makita-UI",
     version="0.1",
-    author="Your Name",
+    author="Jelle Teijema",
     author_email="j.j.teijema@uu.nl",
     description="Experimental UI for the asreview-makita package",
     packages=find_packages(),
     install_requires=[
         "asreview-makita",
         "PySimpleGUI>=4.0.0"
-        # Add any other dependencies here
     ],
     entry_points={
     "console_scripts": [
