52965ef0ddd37a2c9e0dc90b87d72cda4c31e5ac
JT <j.j.teijema@uu.nl>
2023-07-19 23:16:35 +0200

    Update executor

--- makita_ui/entrypoint.py
+++ makita_ui/entrypoint.py
@@ -1,18 +1,52 @@
+import os
 import PySimpleGUI as sg
+from pathlib import Path
+
+from asreviewcontrib.makita.config import TEMPLATES_FP
+from asreviewcontrib.makita.utils import FileHandler
+
 
 class MakitaUI:
     def __init__(self):
-        # Initialization code here
-        from asreviewcontrib.makita.entrypoint import MakitaEntryPoint
+        self.layout = self._create_layout()
 
-    def execute(self):
-        """Run the makita UI."""
-        layout = [
-            [sg.Button("Click Me")],
-            [sg.Text("Hello, GUI!", key="-OUTPUT-")]
+    def _create_layout(self):
+        return [
+            [sg.Button("Template", key="-TEMPLATE-")],
+            [sg.Button("Add Script", key="-ADD-SCRIPT-")],
+            [sg.Text("Output Directory: "), sg.Input(default_text=os.getcwd(), key="-OUTPUT-DIR-"), sg.FolderBrowse(target="-OUTPUT-DIR-")],
+            [sg.Button("Exit", button_color=("white", sg.theme_background_color()), key="-EXIT-", pad=((0, 0), (10, 0)))]
         ]
 
-        window = sg.Window("My GUI", layout)
+    def execute(self):
+        window = sg.Window("Makita UI", self.layout)
+
+        while True:
+            event, values = window.read()
+            if event in (sg.WINDOW_CLOSED, "-EXIT-"):
+                break
+            elif event == "-TEMPLATE-":
+                self._show_template_window()
+            elif event == "-ADD-SCRIPT-":
+                self._show_add_script_window(values["-OUTPUT-DIR-"])
+
+        window.close()
+
+    def _show_template_window(self):
+        template_layout = [
+            # Add template specific elements here
+            [sg.Text("Template Window")],
+            [sg.Button("Back")]
+        ]
+        window = sg.Window("Template", template_layout)
+
+        while True:
+            event, values = window.read()
+            if event == sg.WINDOW_CLOSED or event == "Back":
+                break
+
+        window.close()
+
 
         while True:
             event, values = window.read()
