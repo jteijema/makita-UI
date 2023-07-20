83e882ceb55e76200c50701ab015b5962bfa8b48
JT <j.j.teijema@uu.nl>
2023-07-20 15:44:12 +0200

    Update layout.py

--- makita_ui/layout.py
+++ makita_ui/layout.py
@@ -1,6 +1,7 @@
 import os
 import PySimpleGUI as sg
 import platform
+import pkg_resources
 
 def _mm_parameters(window):
     window["n_runs"].update(visible=False)
@@ -24,27 +25,12 @@ def _main_layout():
     '''Create the layout for the main window.'''
 
     return [            
-        [sg.Text("Makita UI!", font=("Arial", 14, "bold"))],
+        [sg.Text("Welcome to Makita-UI!", font=("Arial", 14, "bold"))],
         [sg.Text("This interface allows you to generate templates and scripts for the ASReview Makita extension.")],
         [sg.Text("Select an option below to get started:")],
         [sg.Button("Template", key="-TEMPLATE-")],
         [sg.Button("Add Script", key="-ADD-SCRIPT-")],
-        [sg.Button("Exit", button_color=("white", sg.theme_background_color()), key="-EXIT-", pad=((0, 0), (10, 0))), sg.Push(), sg.Text("Version 0.3.1", font=("Arial", 8))]
-    ]
-
-def _script_layout(available_scripts):
-    '''Create the layout for the add script window.'''
-
-    return [
-        [sg.Text("Add Script", font=("Arial", 14, "bold"))],
-        [sg.Text("Select Scripts:")],
-        [sg.Listbox(values=available_scripts, key="-SCRIPTS-", size=(30, 6), select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE)],
-        [sg.Text("Scripts will be generated in the output directory.")],
-        [sg.Text("Output", font=("Arial", 14, "bold"))],
-        [sg.Text("Output Directory: ")],
-        [sg.Input(default_text=os.getcwd(), key="-OUTPUT-DIR-"), sg.FolderBrowse(target="-OUTPUT-DIR-")],
-        [sg.Checkbox("Allow Overwrite", key="-OVERWRITE-", default=True, enable_events=True)],
-        [sg.Button("Back", button_color=("white", sg.theme_background_color())), sg.Button("Create")]
+        [sg.Button("Exit", button_color=("white", sg.theme_background_color()), key="-EXIT-", pad=((0, 0), (10, 0))), sg.Push(), sg.Text("Makita-UI v" + pkg_resources.require("Makita-UI")[0].version, font=("Arial", 8))]
     ]
 
 def _template_layout(templates, data_dir):
@@ -53,6 +39,8 @@ def _template_layout(templates, data_dir):
     if os.path.exists(data_dir):
         # load all the files in the data folder
         data_files = '\n'.join([f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))])
+        if len(data_files) == 0:
+            data_files = "No files found in data folder."
         dataFolderString = sg.Multiline(data_files, size=(61, 4), disabled=True, autoscroll=True)
     else:
         dataFolderString = sg.Button("Create Data Folder", key="-CREATE-DATA-FOLDER-")
@@ -76,4 +64,19 @@ def _template_layout(templates, data_dir):
         [dataFolderString],
         [sg.Button("Open Data Folder", key="-OPEN-DATA-FOLDER-", visible=platform.system() == "Windows" and os.path.exists(data_dir))],
         [sg.Button("Back", button_color=("white", sg.theme_background_color()), pad=((0, 0), (10, 0)))],
+    ]
+
+def _script_layout(available_scripts):
+    '''Create the layout for the add script window.'''
+
+    return [
+        [sg.Text("Add Script", font=("Arial", 14, "bold"))],
+        [sg.Text("Select Scripts:")],
+        [sg.Listbox(values=available_scripts, key="-SCRIPTS-", size=(30, 6), select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE)],
+        [sg.Text("Scripts will be generated in the output directory.")],
+        [sg.Text("Output", font=("Arial", 14, "bold"))],
+        [sg.Text("Output Directory: ")],
+        [sg.Input(default_text=os.getcwd(), key="-OUTPUT-DIR-"), sg.FolderBrowse(target="-OUTPUT-DIR-")],
+        [sg.Checkbox("Allow Overwrite", key="-OVERWRITE-", default=True, enable_events=True)],
+        [sg.Button("Back", button_color=("white", sg.theme_background_color())), sg.Button("Create")]
     ]
